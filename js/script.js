bc27b909154ee4f3ac614bb1277a84efda714647
Tithi <77726750+T-Fatema@users.noreply.github.com>
2021-06-27 20:11:59 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay:true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 1,
+            nav: false
+        },
+        1000: {
+            items: 1,
+            nav: false,
+            loop: false
+        }
+    }
+
+});
+
+
+
